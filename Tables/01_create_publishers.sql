2ef5c10ebe18bf23bb50b9ecbf5efb4fd3788c6d
adityajagtap976-collab <adityajagtap976@gmail.com>
2026-09-11 17:51:46 +0530

    Consolidate tables into 01_schema.sql

--- Tables/01_create_publishers.sql
+++
@@ -1,8 +0,0 @@
-create table publishers (
-   publisher_id   number(10) generated always as identity ( start with 1 increment by 1 ) primary key,
-   publisher_name varchar2(150) not null,
-   country        varchar2(80),
-   contact_email  varchar2(150),
-   constraint uq_publisher_name unique ( publisher_name ),
-   constraint uq_publisher_email unique ( contact_email )
-);
